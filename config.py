8abce873d7c6a9d41c6d224f70b24b48a7201662
jhills5821 <44476310+jhills5821@users.noreply.github.com>
2019-05-25 14:21:37 -0700

    first set of files added

---
+++ config.py
@@ -0,0 +1,19 @@
+# etrade sand box info
+
+sandbox_key = "60fffbb238d33e96b53161d7aca7f41a"
+
+sandbox_secret = "7ab57d3d02de8dc849af43eb938279af"
+
+sp = "J5e8h2i1"
+
+consumer_key = "c19bdff978f9a4dc4a887125c9fc515c"
+
+consumer_secret = "1eb030550f09f72302381b1135385ed0"
+
+# IEX keys
+
+IEX_token = "pk_19f5f467e5d3427e821696855a3a98a5"
+
+IEX_secret = "sk_0b1ee3445cad4df2859d0fc1c6ee33b3"
+
+AV_key ="IU020D73RCI8DGLY"
